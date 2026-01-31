d89f04d2b4d571197e76e7e1437e2b19a2dea9e3
Xavier Carbo <70958606+xaviercarbo@users.noreply.github.com>
2026-01-31 14:46:25 +0100

    Update_01

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,4 +1,4 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbxhrJwGOTMQsGhMEurvODftQhK80iMbE9_Ml5_U7P1TnjSekdHFpBc1uNQot5bMrhMPVw/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbxNZiRcnv-SVDWwmfMt5Os5Ba2CakNVcBhfn4_7epzNgtG8KWN_UQGgjrGj4jfuUnwKKQ/exec";
 
 async function fetchDades() {
     try {
